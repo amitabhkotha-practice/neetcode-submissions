52d5f71bf0754ed28e58d12a6231d7f557098547
amitabhkotha-practice <practice.amitabh.kotha0@gmail.com>
2026-09-01 18:56:48 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,54 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node root) {
+        if (root == null)
+            return root;
+        Map<Node, Node> oldToNew = new HashMap<>();
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            Node node = stack.pop();
+            if(oldToNew.containsKey(node))
+                continue;
+            
+            Node newNode = new Node(node.val);
+            oldToNew.put(node, newNode);
+
+            for(Node neighbour: node.neighbors) {
+                stack.push(neighbour);
+            }
+        }
+        // for(Map.Entry<Node, Node> entry: oldToNew.entrySet()) {
+        //     System.out.println("Old Node Val: "+ entry.getKey().val + " New Node Val: "+ entry.getValue().val);
+        // }
+        for(Node node: oldToNew.keySet()) {
+            Node newNode = oldToNew.get(node);
+            if(newNode.neighbors == null) {
+                newNode.neighbors = new ArrayList<Node>();
+            }
+            for(Node neighbour: node.neighbors) {
+                newNode.neighbors.add(oldToNew.get(neighbour));
+            }
+        }
+        return oldToNew.get(root);
+    }
+}
